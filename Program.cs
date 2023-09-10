dc218308bddf1a1597b9cd1a6fed2d704a78abbc
Mohammad Khaled Safi <51378060+MohammedSafi96@users.noreply.github.com>
2023-09-10 14:16:17 +0300

    push 2

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+          * Assuming Singleton is created from client 1 class
+          * we refer to the GetInstance property from the Singleton class
+          */
+using SingletonDesignPattern;
+
+Parallel.Invoke(
+              () => Client1(),
+              () => Client2());
+Console.ReadLine();
+
+static void Client1()
+{
+    /*
+     * Assuming Singleton is created from client 1 class
+     * we refer to the GetInstance property from the Singleton class
+     */
+    Singleton client1 = Singleton.GetInstance;
+    client1.PrintDetails("From Client 1");
+}
+
+static void Client2()
+{
+    /*
+     * Assuming Singleton is created from client 2 class
+     * we refer to the GetInstance property from the Singleton class
+     */
+    Singleton client2 = Singleton.GetInstance;
+    client2.PrintDetails("From Client 2");
+}
---
+++ Singleton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    /*
+    *  Sealed ensures the class being inherited and
+    *  object instantiation is restricted in the derived class
+    */
+    public sealed class Singleton
+    {
+        /*
+         * Private property initilized with null
+         * ensures that only one instance of the object is created
+         * based on the null condition
+         */
+        private static Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton()); // Lazy loading initialization
+        //private static Singleton instance = new Singleton(); // Eager loading initialization
+
+        private static readonly object obj = new object();
+        /*
+         * public property is used to return only one instance of the class
+         * leveraging on the private property
+         */
+        public static Singleton GetInstance
+        {
+            get
+            {
+                // loading initialization
+                //lock (obj)
+                //{
+                //    if (instance == null)
+                //        instance = new Singleton();
+                //}
+                return instance.Value;
+            }
+        }
+        /*
+         * Private constructor ensures that object is not
+         * instantiated other than with in the class itself
+         */
+        private Singleton()
+        {
+            Console.WriteLine("A new instance has been created!");
+        }
+        /*
+         * Public method which can be invoked through the singleton instance
+         */
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
